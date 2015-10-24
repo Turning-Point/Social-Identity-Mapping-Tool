bf42ef7fd7e2ccd2d8ac42fad6cf8dd88d61ac20
Tim Osborn <tim@memelab.com.au>
2015-10-24 16:47:31 +1100

    WIP begin bar graphs

--- index.js
+++ index.js
@@ -12,11 +12,31 @@ d3.json('./data.json', function (data) {
 
 	var svg = d3.select('svg')
     .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('rect')
-      .attr('class', 'inner')
+    .attr('height', height + margin.top + margin.bottom);
+
+  svg.append('rect')
+      .attr('class', 'background')
       .attr('width', width)
       .attr('height', height)
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  var groups = svg.append('g')
+    .attr('class', 'groups');
+
+  var group = groups.selectAll('.group')
+    .data(data.groups);
+
+  // enter
+  var groupInner = group.enter().append('g')
+    .attr('class', 'group')
+    .attr('transform', function(d) {
+      return 'translate(' + 100 + ',' + 100 + ')';
+    })
+    .append('rect')
+      .attr('class', 'group__background')
+      .attr('width', 200)
+      .attr('height', 100);
+
+
 
 });
