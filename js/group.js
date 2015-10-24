26cc17650be5bbfa218884f27469abad7ea4e854
Tim Osborn <tim@memelab.com.au>
2015-10-25 00:51:55 +1100

    fix data structure to make behaviours accessible

--- js/group.js
+++ js/group.js
@@ -22,7 +22,38 @@ export default function group(parent, data) {
       .attr('width', WIDTH)
       .attr('height', HEIGHT);
 
+  groupInner.call(barComponent);
+}
+
+
+function barComponent(parent) {
+
+  const barGroup = parent.append('g')
+    .attr('class', 'barGroup');
+
+  const bar = barGroup.selectAll('.group__bar')
+    .data( d => {
+      console.log(d.behaviours.alcohol);
+      return d.behaviours.alcohol;
+    });
+
+  // enter
+  bar.enter().append('rect')
+    .attr('class', 'group__bar');
+
+  // update
+  bar.attr('x', (d, i) => mapCumulativeBars )
+    .attr('y', 0)
+    .attr('width', 20)
+    .attr('height', 30)
+    .style('fill', 'red');
+}
 
 export function toggleAod() {
   console.log('toggleAod');
+}
+
+fucntion mapCumulativeBars(d, i) {
+  console.log('d,i', d,i);
+  return d[Object.keys(d)] * 20;
 }
